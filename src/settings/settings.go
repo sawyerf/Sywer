c9899bed89bf3f18c27f6f1c356cf9558d33a3f5
Sawyer Flink <sawyer.flink@protonmail.ch>
2018-03-02 17:29:20 +0100

    Fix Bug

--- src/settings/settings.go
+++ src/settings/settings.go
@@ -11,6 +11,27 @@ type Settings struct{
 	Error301 string
 }
 
+func Line_analyzer(set Settings, data string, i int) Settings{
+	if data[0:5] == "port	"{
+		set.Port = data[5:i]
+	} else if data[0:5] == "path	"{
+		set.Path = data[5:i]
+	} else if data[0:6] == "index	"{
+		set.Index = data[6:i]
+	} else if data[0:6] == "error_"{
+		switch data[6:10] {
+		case "404	":
+			set.Error404 = data[10:i]
+		case "301	":
+			set.Error301 = data[10:i]
+		default:
+			fmt.Println("[!] This type of error is not understand : \"" + data[0:i])
+			os.Exit(0)
+		}
+	}
+	return set
+}
+
 func Recup(path string) Settings{
 	var set Settings
 	//Open file
@@ -23,33 +44,23 @@ func Recup(path string) Settings{
 	buffer := make([]byte, siz)
 	file, _ := os.Open(path)
 	size, _ := file.Read(buffer)
-	var data string = string(buffer[:size]) + "       "
-
+	var data string = string(buffer[:size])
 	nb := 0
-	fmt.Println(data)
-	for i:=0; i!=size; i++{
-		if data[i] == 13{
-			if data[nb:nb+5] == "port	"{
-				set.Port = data[nb+5:i]
-			} else if data[nb:nb+5] == "path	"{
-				set.Path = data[nb+5:i]
-			} else if data[nb:nb+6] == "index	"{
-				set.Index = data[nb+6:i]
-			} else if data[nb:nb+6] == "error_"{
-				switch data[nb+6:nb+10] {
-				case "404	":
-					set.Error404 = data[nb+10:i]
-				case "301	":
-					set.Error301 = data[nb+10:i]
-				default:
-					fmt.Println("[!] This type of error is not understand : \"" + data[nb:i])
-					os.Exit(0)}
-			}
+	for i := 0; i!=size; i++{
+		if data[i] == 13 {
+			set = Line_analyzer(set, data[nb:i], i-nb)
 			nb = i + 2
+			i++
+		} else if data[i] == 10{
+			set = Line_analyzer(set, data[nb:i], i-nb)
+			nb = i + 1
 		}
 	}
 	if set.Port == ""{
 		set.Port = "80"
+	}
+	if set.Index == ""{
+		set.Index =	 "index.html"
 	}
 	fmt.Println(set)
 	return set
