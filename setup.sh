1cb2e973a7f878e78a4a8668c71a8c6e0a1aa4d7
Sawyer Flink <sawyer.flink@protonmail.ch>
2018-03-10 20:18:38 +0100

    Fix Bug

--- setup.sh
+++ setup.sh
@@ -4,10 +4,9 @@ mkdir -p /var/log/sywer/
 mkdir -p /var/lib/sywer/
 
 go build Sywer.go
-chmod +x /ressources/sywer
+chmod +x ressources/sywer
 
 
 mv Sywer /var/lib/sywer/sywer
-cp /ressources/sywer /usr/sbin/sywer
+cp ressources/sywer /usr/sbin/sywer
 cp ressources/sywer.service /etc/systemd/system/.
-cp ressources/settings.swy /var/lib/sywer
